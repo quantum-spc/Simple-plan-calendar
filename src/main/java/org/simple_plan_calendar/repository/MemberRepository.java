a691dcc28fb6c992075cf35e46068f397aa59d66
qtm <zmxnxnxn1@naver.com>
2022-10-07 13:05:43 +0900

    미사용 파일 삭제

--- src/main/java/org/simple_plan_calendar/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package org.simple_plan_calendar.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.simple_plan_calendar.entity.Member;
-
-public interface MemberRepository extends JpaRepository<Member, String> {
-}
