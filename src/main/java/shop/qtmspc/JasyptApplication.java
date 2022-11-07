23b029b68a02c5ac00905a3841c6bfcdebad60bd
qtm <zmxnxnxn1@naver.com>
2022-11-07 16:36:40 +0900

    데이터베이스 접속정보 암호화 비밀번호 파일 별도 처리

--- src/main/java/shop/qtmspc/JasyptApplication.java
+++ src/main/java/shop/qtmspc/JasyptApplication.java
@@ -1,17 +1,19 @@
-package shop.qtmspc.config;
+package shop.qtmspc;
 
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * 암호화 텍스트 생성 APP
  */
 //@SpringBootApplication
+@PropertySource("classpath:jasyptPassword.properties")
 public class JasyptApplication implements CommandLineRunner {
 
-    @Value("${jasypt.encryptor.password}")
+    @Value("${jasypt.encryptor.password.ignore}")
     private String encryptKey;
 
     public static void main(String[] args) {
--- src/main/java/shop/qtmspc/config/JasyptConfig.java
+++ src/main/java/shop/qtmspc/config/JasyptConfig.java
@@ -6,19 +6,24 @@ import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * 암호화 텍스트 설정 파일
  */
 @Configuration
+@PropertySource("classpath:jasyptPassword.properties")
 public class JasyptConfig {
 
-    @Value("${jasypt.encryptor.password}")
+    //@Value("${jasypt.encryptor.password}")
+    //private String encryptKey;
+    //암호화 키값이 application.properties 에 들어있는 보안문제로 사용 안함
+
+    @Value("${jasypt.encryptor.password.ignore}")
     private String encryptKey;
 
     @Bean(name = "jasyptStringEncryptor")
     public StringEncryptor stringEncryptor() {
-
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
         config.setPassword(encryptKey); // 암호화할 때 사용하는 키
