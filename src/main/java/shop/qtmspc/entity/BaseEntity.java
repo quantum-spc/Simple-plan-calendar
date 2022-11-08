5612046607d1985cab1f474ad9475a97bf2dd790
qtm <zmxnxnxn1@naver.com>
2022-11-08 15:16:04 +0900

    필요없는 애노테이션 제거 생성자 관련 주석 추가

--- src/main/java/shop/qtmspc/entity/BaseEntity.java
+++ src/main/java/shop/qtmspc/entity/BaseEntity.java
@@ -4,15 +4,13 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
-import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
-@EntityListeners(value = { AuditingEntityListener.class })
+//@EntityListeners(value = { AuditingEntityListener.class })
 @Getter
 @Setter
 abstract class BaseEntity {
--- src/main/java/shop/qtmspc/entity/User.java
+++ src/main/java/shop/qtmspc/entity/User.java
@@ -8,8 +8,8 @@ import javax.persistence.*;
 
 @Entity
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
+@AllArgsConstructor // 올 아규먼츠 컨스트럭터 -> 파라미터가 있는 기본 생성자만 만들어 줌
+@NoArgsConstructor // 노 아규먼츠 컨스트럭터 -> 파라미터가 없는 기본 생성자만 만들어 줌
 @Getter
 @Setter
 @ToString
