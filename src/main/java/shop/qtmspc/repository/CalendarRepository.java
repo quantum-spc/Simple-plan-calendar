68af5903314a7685a681855bc4ca14486376846f
qtm <zmxnxnxn1@naver.com>
2022-11-08 16:29:37 +0900

    QueryDSL 테스트 코드 적용

--- src/main/java/shop/qtmspc/repository/CalendarRepository.java
+++ src/main/java/shop/qtmspc/repository/CalendarRepository.java
@@ -1,12 +1,13 @@
 package shop.qtmspc.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import shop.qtmspc.entity.Calendar;
 import shop.qtmspc.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface CalendarRepository extends JpaRepository<Calendar, Long> {
+public interface CalendarRepository extends JpaRepository<Calendar, Long>, QuerydslPredicateExecutor<Calendar> {
 
     List<Calendar> findAllByUserAndDelflag(User user, String delflag);
 }
--- src/test/java/shop/qtmspc/repository/CalendarRepositoryTest.java
+++ src/test/java/shop/qtmspc/repository/CalendarRepositoryTest.java
@@ -1,11 +1,18 @@
 package shop.qtmspc.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import shop.qtmspc.entity.Calendar;
-import shop.qtmspc.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import shop.qtmspc.entity.Calendar;
+import shop.qtmspc.entity.QCalendar;
+import shop.qtmspc.entity.User;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -49,6 +56,24 @@ class CalendarRepositoryTest {
                 calendar -> System.out.println(calendar)
         );
 
+    }
+
+    @Test
+    @DisplayName("QueryDSL 테스트")
+    public void SelectUserById() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("id").descending());
+
+        QCalendar qCalendar = QCalendar.calendar;
+
+        BooleanBuilder builder = new BooleanBuilder();
+        BooleanExpression expression = qCalendar.title.contains("TEST");
+        builder.and(expression);
+
+        Page<Calendar> result = calendarRepository.findAll(builder, pageable);
+
+        result.stream().forEach(calendar -> {
+            System.out.println(calendar);
+        });
     }
 
 }
