39fe7311ff785caafc881df3fee928d89b8626b4
qtm <zmxnxnxn1@naver.com>
2022-11-17 15:00:57 +0900

    비밀번호 암호화 테스트 코드 검증 수정

--- src/test/java/shop/qtmspc/repository/UserRepositoryTest.java
+++ src/test/java/shop/qtmspc/repository/UserRepositoryTest.java
@@ -2,6 +2,7 @@ package shop.qtmspc.repository;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.session.SqlSession;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,8 +43,8 @@ class UserRepositoryTest {
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
         // then
-        assertThat(rawPassword).isNotEqualTo(encodedPassword);
-        assertThat(passwordEncoder.matches(rawPassword, encodedPassword)).isTrue();
+        boolean matchResult = passwordEncoder.matches(rawPassword, encodedPassword);
+        Assertions.assertThat(matchResult).isTrue();
     }
 
     @Test
