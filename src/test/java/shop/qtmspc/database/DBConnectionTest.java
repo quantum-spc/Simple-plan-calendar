00adfdd1affcd01b8fd518bb050ff6043b59b588
qtm <zmxnxnxn1@naver.com>
2022-11-19 17:17:30 +0900

    데이터베이스 연결 테스트 코드 생성

---
+++ src/test/java/shop/qtmspc/database/DBConnectionTest.java
@@ -0,0 +1,46 @@
+package shop.qtmspc.database;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class DBConnectionTest {
+
+    @Value("${spring.datasource.url}")
+    private String URL;
+
+    @Value("${spring.datasource.username}")
+    private String USERNAME;
+
+    @Value("${spring.datasource.password}")
+    private String PASSWORD;
+
+    @Test
+    @DisplayName("데이터베이스 연결 테스트")
+    void connection() {
+        String resultText = "fail";
+        try {
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            if (connection != null) {
+                System.out.println("connection = " + connection);
+                resultText = "success";
+            }
+        } catch(Exception e) {
+            System.out.println("e = " + e);
+            if (e.toString().contains("max_user_connections")) { // 최대 연결 허용수 제한으로 나오지만 정상
+                resultText = "success";
+            } else {
+                System.out.println("커넥션 연결 실패");
+            }
+        }
+        assertThat(resultText).isEqualTo("success");
+        
+    }
+}
