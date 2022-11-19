50d113ef72ab36d7d4c87f3ea0df82901d7448da
qtm <zmxnxnxn1@naver.com>
2022-11-19 17:17:12 +0900

    스프링 시큐리티 설정파일 변경

--- src/main/java/shop/qtmspc/config/WebSecurityConfig.java
+++ src/main/java/shop/qtmspc/config/WebSecurityConfig.java
@@ -4,26 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors().disable()
-                .csrf().disable()
-                .formLogin().disable()
-                .headers().frameOptions().disable();
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().disable();
+        http.csrf().disable();
+        http.formLogin().disable();
+        http.headers().frameOptions().disable();
+
+        return http.build();
     }
 }
