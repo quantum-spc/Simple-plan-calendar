bb5f79ef59926d27b8d1475b30d88fe096cce845
qtm <114331632+quantum-spc@users.noreply.github.com>
2022-12-11 18:35:27 +0900

    Update ddl.sql

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -2,14 +2,13 @@ CREATE TABLE `plan_calendar` (
      `id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '시퀀스 번호',
      `regdate` DATETIME NULL DEFAULT NULL COMMENT '등록 날짜',
      `delflag` VARCHAR(2) NULL DEFAULT 'N' COMMENT '삭제 여부',
-     `user_id` BIGINT(20) NULL DEFAULT NULL COMMENT '유저 seq',
+     `user_id` BIGINT(20) NULL DEFAULT NULL COMMENT '유저 번호',
      `start` DATE NULL DEFAULT NULL COMMENT '시작 날짜',
      `end` DATE NULL DEFAULT NULL COMMENT '종료 날짜',
      `title` VARCHAR(255) NULL DEFAULT NULL COMMENT '제목',
      `color` VARCHAR(20) NULL DEFAULT NULL COMMENT '배경 색깔',
      `moddate` DATETIME NULL DEFAULT NULL,
-     PRIMARY KEY (`id`) USING BTREE,
-     INDEX `FK6noekxi6fr5or5e8dyhdjnpto` (`user_id`) USING BTREE
+     PRIMARY KEY (`id`) USING BTREE
 )
 ;
 
